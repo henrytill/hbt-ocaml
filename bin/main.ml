fa1101ef6f261bc82bb964d75c6c635f7225e4e4
Henry Till <henrytill@gmail.com>
2023-10-31 00:33:39 -0700

    main: use Format module

--- bin/main.ml
+++ bin/main.ml
@@ -24,23 +24,37 @@ let find_xml_files dir =
   in
   go []
 
+let pp_list pp_val fmt xs =
+  Format.pp_print_char fmt '[';
+  Format.pp_print_list ~pp_sep:(fun fmt _ -> Format.pp_print_string fmt "; ") pp_val fmt xs;
+  Format.pp_print_char fmt ']'
+
+let pp_string_list fmt xs =
+  let pp_string fmt s = Format.fprintf fmt "\"%s\"" s in
+  pp_list pp_string fmt xs
+
 let () =
+  let fmt = Format.std_formatter in
   let data_dir = Config.get_data_dir () in
   if not (Sys.file_exists data_dir && Sys.is_directory data_dir) then
     Sys.mkdir data_dir 0o700;
-  Printf.printf "data_dir: %s\n" data_dir;
+  Format.fprintf fmt "data_dir: %s\n" data_dir;
   let import_dir = Config.get_import_dir () in
+  if not (Sys.file_exists import_dir && Sys.is_directory import_dir) then
+    Sys.mkdir import_dir 0o700;
   let xml_files = find_xml_files import_dir in
-  let joined = "\"" ^ String.concat "\"; \"" xml_files ^ "\"" in
-  Printf.printf "xml_files: [%s]\n" joined;
+  Format.pp_print_string fmt "xml_files: ";
+  pp_string_list fmt xml_files;
+  Format.pp_print_newline fmt ();
   let posts = Importer.parse_xml (List.hd xml_files) in
-  Printf.printf "parsed: %d posts, " (List.length posts);
+  Format.fprintf fmt "parsed: %d posts, " (List.length posts);
   let open Importer in
   let tags =
     List.fold_left
       (fun acc post -> StringSet.of_list post.tag |> StringSet.union acc)
       StringSet.empty posts
   in
-  Printf.printf "%d tags\n" (StringSet.cardinal tags);
-  let joined = "\"" ^ String.concat "\"; \"" (StringSet.elements tags) ^ "\"" in
-  Printf.printf "tags: [%s]\n" joined
+  Format.fprintf fmt "%d tags\n" (StringSet.cardinal tags);
+  Format.pp_print_string fmt "tags: ";
+  pp_string_list fmt (StringSet.elements tags);
+  Format.pp_print_newline fmt ()
