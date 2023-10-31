0c76403b4e55a3377492d0a13c75ebad69a053f4
Henry Till <henrytill@gmail.com>
2023-10-31 16:12:11 -0700

    add flake.{nix,lock}

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
+  };
+  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
+    let package = "backlogged";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        scope = on.buildOpamProject { } package ./. { ocaml-base-compiler = "4.14.1"; };
+        overlay = final: prev: {};
+      in {
+        legacyPackages = scope.overrideScope' overlay;
+
+        packages.default = self.legacyPackages.${system}.${package};
+      });
+}
