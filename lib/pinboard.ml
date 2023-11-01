b9b3cf0e67cde651c90da63f0bf7e2c4c07a6810
Henry Till <henrytill@gmail.com>
2023-10-31 20:43:04 -0700

    refactor Pinboard.from_xml

--- lib/pinboard.ml
+++ lib/pinboard.ml
@@ -31,47 +31,46 @@ let pp fmt p =
 
 let to_string p = Format.asprintf "%a" pp p
 
-let parse_post attrs =
-  let get_attr k =
-    try
-      let k = (String.empty, k) in
-      List.assoc k attrs
-    with Not_found -> String.empty
-  in
-  let get_attr_option k =
-    let s = get_attr k in
-    Option.(if String.empty = s then None else Some s)
-  in
-  {
-    href = get_attr "href";
-    time = get_attr "time";
-    description = get_attr_option "description";
-    extended = get_attr_option "extended";
-    tag = Str.split (Str.regexp "[ \t]+") (get_attr "tag");
-    hash = get_attr "hash";
-    shared = get_attr "shared" = "yes";
-    toread = get_attr "toread" = "yes";
-  }
-
 let from_xml file =
   let ic = In_channel.open_text file in
   let xml = Xmlm.make_input (`Channel ic) in
-  let rec go depth posts =
+  let to_t attrs =
+    let get_attr k =
+      try
+        let k = (String.empty, k) in
+        List.assoc k attrs
+      with Not_found -> String.empty
+    in
+    let get_attr_option k =
+      let s = get_attr k in
+      Option.(if String.empty = s then None else Some s)
+    in
+    let href = get_attr "href" in
+    let time = get_attr "time" in
+    let description = get_attr_option "description" in
+    let extended = get_attr_option "extended" in
+    let tag = Str.split (Str.regexp "[ \t]+") (get_attr "tag") in
+    let hash = get_attr "hash" in
+    let shared = get_attr "shared" = "yes" in
+    let toread = get_attr "toread" = "yes" in
+    { href; time; description; extended; tag; hash; shared; toread }
+  in
+  let rec go depth acc =
     match Xmlm.input xml with
     | `El_start ((_, "post"), attrs) ->
-        let post = parse_post attrs in
-        go (depth + 1) (post :: posts)
-    | `El_start ((_, "posts"), _) -> go (depth + 1) posts
+        let parsed = to_t attrs in
+        go (depth + 1) (parsed :: acc)
+    | `El_start ((_, "posts"), _) -> go (depth + 1) acc
     | `El_start ((_, s), _) -> failwith ("unexpected El_start: " ^ s)
-    | `El_end when depth = 1 -> posts
-    | `El_end -> go (depth - 1) posts
-    | `Data _ -> go depth posts
-    | `Dtd _ -> go depth posts
+    | `El_end when depth = 1 -> acc
+    | `El_end -> go (depth - 1) acc
+    | `Data _ -> go depth acc
+    | `Dtd _ -> go depth acc
   in
-  let posts = go 0 [] in
+  let ret = go 0 [] in
   if not (Xmlm.eoi xml) then invalid_arg "document not well-formed";
-  close_in ic;
-  posts
+  In_channel.close ic;
+  ret
 
 let from_json file =
   let json = Yojson.Basic.from_file file in
