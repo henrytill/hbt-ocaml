c6f1e6393105157ad3b7ac87958e4ba9bd97446a
Henry Till <henrytill@gmail.com>
2023-11-03 01:22:14 -0700

    Pinboard: refine html parsing helpers

--- lib/pinboard.ml
+++ lib/pinboard.ml
@@ -100,67 +100,67 @@ let from_xml file =
   close_in ic;
   ret
 
-let maybe_start_dd stream ~on_failure on_success =
+let maybe_start stream name ~on_failure on_success =
   match Markup.peek stream with
-  | Some (`Start_element ((_, "dd"), _)) ->
+  | Some (`Start_element ((_, n), attrs)) when n = name ->
       ignore (Markup.next stream);
-      on_success ()
-  | _ -> on_failure ()
+      on_success attrs
+  | e -> on_failure e
 
-let maybe_text stream ~on_failure on_success =
+let maybe_text stream ?(p = fun _ -> true) ~on_failure on_success =
   match Markup.peek stream with
-  | Some (`Text xs) ->
+  | Some (`Text xs) when p xs ->
       ignore (Markup.next stream);
       on_success xs
-  | _ -> on_failure ()
+  | e -> on_failure e
 
-let next_start_a stream ?(on_failure = fun () -> failwith "parse error") on_success =
+let next_start stream name ?(on_failure = fun _ -> failwith "parse error") on_success =
   match Markup.next stream with
-  | Some (`Start_element ((_, "a"), attrs)) -> on_success attrs
-  | _ -> on_failure ()
+  | Some (`Start_element ((_, n), attrs)) when n = name -> on_success attrs
+  | e -> on_failure e
 
-let next_text stream ?(on_failure = fun () -> failwith "parse error") on_success =
+let next_text stream ?(on_failure = fun _ -> failwith "parse error") on_success =
   match Markup.next stream with
   | Some (`Text xs) -> on_success xs
-  | _ -> on_failure ()
+  | e -> on_failure e
 
-let next_end stream ?(on_failure = fun () -> failwith "parse error") on_success =
+let next_end stream ?(on_failure = fun _ -> failwith "parse error") on_success =
   match Markup.next stream with
   | Some `End_element -> on_success ()
-  | _ -> on_failure ()
+  | e -> on_failure e
 
 let is_newline xs =
-  let s = String.concat "" xs in
-  Str.string_match (Str.regexp "^[\n\r]*$") s 0
+  let s = String.concat String.empty xs in
+  Str.string_match (Str.regexp "[\n\r]*") s 0
 
 let skip_newlines stream () =
-  match Markup.peek stream with
-  | Some (`Text xs) when is_newline xs -> ignore (Markup.next stream)
-  | _ -> ()
+  let f _ = () in
+  maybe_text stream ~p:is_newline ~on_failure:f f
+
+let tag s = ((String.empty, s), [])
+let skip stream t = if Markup.peek stream = Some t then ignore (Markup.next stream) else ()
 
 let from_html file =
   let ic = open_in file in
   let channel = Markup.channel ic in
   let html = Markup.parse_html channel in
   let signals = Markup.signals html in
-  let tag s = ((String.empty, s), []) in
-  let skip t = if Markup.peek signals = Some t then ignore (Markup.next signals) else () in
   let parse_dd () =
-    maybe_start_dd signals ~on_failure:(fun () -> None) @@ fun () ->
+    maybe_start signals "dd" ~on_failure:(fun _ -> None) @@ fun _ ->
     next_text signals @@ fun xs ->
-    next_end signals @@ fun () -> Some (String.concat String.empty xs)
+    next_end signals @@ fun () -> Some (String.trim (String.concat String.empty xs))
   in
   let to_t () =
-    skip (`Start_element (tag "p"));
-    let attrs = next_start_a signals (fun x -> x) in
+    skip signals (`Start_element (tag "p"));
+    let attrs = next_start signals "a" (fun x -> x) in
     let description =
       maybe_text signals
-        ~on_failure:(fun () -> None)
+        ~on_failure:(fun _ -> None)
         (fun xs -> Some (String.trim (String.concat String.empty xs)))
     in
-    skip `End_element (* </a> *);
+    skip signals `End_element (* </a> *);
     skip_newlines signals ();
-    skip `End_element (* implicit </dt> *);
+    skip signals `End_element (* implicit </dt> *);
     let extended = Option.map String.trim (parse_dd ()) in
     let href = get_attr attrs "href" in
     let time = get_attr attrs "add_date" in
