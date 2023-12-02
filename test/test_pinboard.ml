6f32d1f04a678e3e0828f16d255ae8b7b88d8f54
Henry Till <henrytill@gmail.com>
2023-12-01 17:07:22 -0800

    Test_pinboard: tweak

--- test/test_pinboard.ml
+++ test/test_pinboard.ml
@@ -16,26 +16,26 @@ let small =
 </DL></p>
 |}
 
-let small_expected =
-  [
-    Pinboard.make ~href:"http://c-faq.com/decl/spiral.anderson.html" ~time:"1653114361" ~shared:true
-      ~toread:false ~tag:[ "c"; "c++" ] ~description:"Clockwise/Spiral Rule" ();
-    Pinboard.make ~href:"https://docs.microsoft.com/en-us/sysinternals/downloads/procmon"
-      ~time:"1606184699" ~shared:false ~toread:false ~tag:[ "windows-dev" ]
-      ~description:"Process Monitor - Windows Sysinternals | Microsoft Docs"
-      ~extended:"Monitor file system, Registry, process, thread and DLL activity in real-time."
-      ();
-    Pinboard.make
-      ~href:
-        "https://www.intel.com/content/www/us/en/developer/tools/oneapi/vtune-profiler.html#gs.x8oazh"
-      ~time:"1649855530" ~shared:false ~toread:true
-      ~tag:[ "performance"; "profiling"; "tools" ]
-      ~description:"Fix Performance Bottlenecks with Intel® VTune™ Profiler" ();
-  ]
-
-let small_actual = Common.with_temp_file small @@ fun filename -> Pinboard.from_html filename
-
 let test_small () =
-  Alcotest.(check (list pinboard_testable)) "same list" (List.rev small_expected) small_actual
+  let expected =
+    List.rev
+      [
+        Pinboard.make ~href:"http://c-faq.com/decl/spiral.anderson.html" ~time:"1653114361"
+          ~shared:true ~toread:false ~tag:[ "c"; "c++" ] ~description:"Clockwise/Spiral Rule" ();
+        Pinboard.make ~href:"https://docs.microsoft.com/en-us/sysinternals/downloads/procmon"
+          ~time:"1606184699" ~shared:false ~toread:false ~tag:[ "windows-dev" ]
+          ~description:"Process Monitor - Windows Sysinternals | Microsoft Docs"
+          ~extended:"Monitor file system, Registry, process, thread and DLL activity in real-time."
+          ();
+        Pinboard.make
+          ~href:
+            "https://www.intel.com/content/www/us/en/developer/tools/oneapi/vtune-profiler.html#gs.x8oazh"
+          ~time:"1649855530" ~shared:false ~toread:true
+          ~tag:[ "performance"; "profiling"; "tools" ]
+          ~description:"Fix Performance Bottlenecks with Intel® VTune™ Profiler" ();
+      ]
+  in
+  let actual = Common.with_temp_file small @@ fun filename -> Pinboard.from_html filename in
+  Alcotest.(check (list pinboard_testable)) "same list" expected actual
 
 let tests = [ ("from_html", [ Alcotest.test_case "test_small" `Quick test_small ]) ]
