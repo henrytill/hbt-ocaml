e12dc82ec0ab6afd1bb6cea992e82a4d7f89733b
Henry Till <henrytill@gmail.com>
2024-06-29 13:43:06 -0700

    reformat {default,flake}.nix

--- flake.nix
+++ flake.nix
@@ -11,17 +11,31 @@
       flake = false;
     };
   };
-  outputs = { self, flake-utils, opam-nix, nixpkgs, ... }@inputs:
-    let package = "hbt";
-    in flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      flake-utils,
+      opam-nix,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      package = "hbt";
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         on = opam-nix.lib.${system};
-        scope = on.buildOpamProject { resolveArgs.with-test = true; } package ./. { ocaml-base-compiler = "4.14.1"; };
-        overlay = final: prev: {};
-      in {
+        scope = on.buildOpamProject { resolveArgs.with-test = true; } package ./. {
+          ocaml-base-compiler = "4.14.1";
+        };
+        overlay = final: prev: { };
+      in
+      {
         legacyPackages = scope.overrideScope' overlay;
 
         packages.default = self.legacyPackages.${system}.${package};
-      });
+      }
+    );
 }
