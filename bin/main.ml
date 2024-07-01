8df3559ed8a5c71576e650c6631c48a069bf1aa6
Henry Till <henrytill@gmail.com>
2024-06-30 22:08:16 -0700

    main: rework parse functions

--- bin/main.ml
+++ bin/main.ml
@@ -22,6 +22,16 @@ let find_files suffix dir =
   in
   go []
 
+let parse dir suffix parse_fn =
+  let files = find_files suffix dir in
+  let posts = parse_fn (List.hd files) in
+  let tags = Pinboard.tags posts in
+  (files, posts, tags)
+
+let parse_xml dir = parse dir "xml" Pinboard.from_xml
+let parse_html dir = parse dir "html" Pinboard.from_html
+let parse_json dir = parse dir "json" Pinboard.from_json
+
 let pp_list pp_val fmt xs =
   let open Format in
   let pp_sep fmt () = pp_print_string fmt ";@ " in
@@ -31,52 +41,36 @@ let pp_string_list fmt xs =
   let pp_string fmt = Format.fprintf fmt "%S" in
   pp_list pp_string fmt xs
 
-let parse_xml fmt import_dir =
+let print_posts fmt posts tags =
   let open Format in
-  let xml_files = find_files "xml" import_dir in
-  let posts = Pinboard.from_xml (List.hd xml_files) in
-  let tags = Pinboard.tags posts in
-  fprintf fmt "@[xml_files: %a@]@;" pp_string_list xml_files;
   fprintf fmt "@[parsed: %d posts, %d tags@]@;" (List.length posts) (Pinboard.Tags.cardinal tags);
-  fprintf fmt "@[tags: %a@]@;" Pinboard.Tags.pp tags;
-  tags
+  fprintf fmt "@[tags: %a@]@;" Pinboard.Tags.pp tags
 
-let parse_html fmt import_dir =
-  let open Format in
-  let html_files = find_files "html" import_dir in
-  let posts = Pinboard.from_html (List.hd html_files) in
-  let tags = Pinboard.tags posts in
-  fprintf fmt "@[html_files: %a@]@;" pp_string_list html_files;
-  fprintf fmt "@[parsed: %d posts, %d tags@]@;" (List.length posts) (Pinboard.Tags.cardinal tags);
-  fprintf fmt "@[tags: %a@]@;" Pinboard.Tags.pp tags;
-  tags
-
-let parse_json fmt import_dir =
-  let open Format in
-  let json_files = find_files "json" import_dir in
-  let posts = Pinboard.from_json (List.hd json_files) in
-  let tags = Pinboard.tags posts in
-  fprintf fmt "@[json_files: %a@]@;" pp_string_list json_files;
-  fprintf fmt "@[parsed: %d posts, %d tags@]@;" (List.length posts) (Pinboard.Tags.cardinal tags);
-  fprintf fmt "@[tags: %a@]@;" Pinboard.Tags.pp tags;
-  fprintf fmt "@[sample: %a@]@;" Pinboard.pp (List.hd posts);
-  tags
+let mkdir_p dir =
+  if not (Sys.file_exists dir && Sys.is_directory dir) then
+    Sys.mkdir dir 0o700
 
 let () =
   let fmt = Format.std_formatter in
   let data_dir = Config.get_data_dir () in
-  if not (Sys.file_exists data_dir && Sys.is_directory data_dir) then
-    Sys.mkdir data_dir 0o700;
+  mkdir_p data_dir;
   Format.fprintf fmt "@[data_dir: %S@]@;" data_dir;
   let import_dir = Config.get_import_dir () in
-  if not (Sys.file_exists import_dir && Sys.is_directory import_dir) then
-    Sys.mkdir import_dir 0o700;
+  mkdir_p import_dir;
   (* xml *)
-  ignore (parse_xml fmt import_dir);
+  let xml_files, xml_posts, xml_tags = parse_xml import_dir in
+  Format.fprintf fmt "@[xml_files: %a@]@;" pp_string_list xml_files;
+  print_posts fmt xml_posts xml_tags;
   (* html *)
-  let html_tags = parse_html fmt import_dir in
+  let html_files, html_posts, html_tags = parse_html import_dir in
+  Format.fprintf fmt "@[html_files: %a@]@;" pp_string_list html_files;
+  print_posts fmt html_posts html_tags;
   (* json *)
-  let json_tags = parse_json fmt import_dir in
+  let json_files, json_posts, json_tags = parse_json import_dir in
+  Format.fprintf fmt "@[json_files: %a@]@;" pp_string_list json_files;
+  print_posts fmt json_posts json_tags;
+  Format.fprintf fmt "@[sample: %a@]@;" Pinboard.pp (List.hd json_posts);
+  (* tags diff *)
   let diff = Pinboard.Tags.diff html_tags json_tags in
   Format.fprintf fmt "@[diff: %a@]@;" Pinboard.Tags.pp diff;
   Format.pp_print_flush fmt ()
