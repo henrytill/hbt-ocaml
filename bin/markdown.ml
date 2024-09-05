c31bb7316d7c991b451286ebfec7d8ded0427c18
Henry Till <henrytill@gmail.com>
2024-09-05 00:42:07 -0700

    bin: tweak markdown

--- bin/markdown.ml
+++ bin/markdown.ml
@@ -16,27 +16,30 @@ let read_file file =
 
 let () =
   let dump_entities = ref false in
-  let file = ref String.empty in
-  let usage_string = "Usage: " ^ Sys.argv.(0) ^ "[options...] <file>" in
+  let file = ref None in
+  let usage_string = "Usage: " ^ Sys.argv.(0) ^ " [OPTIONS...] <FILE>" in
   let opt_list = [ ("-dump", Arg.Set dump_entities, "dump entities") ] in
-  let process_arg arg = file := arg in
+  let process_arg arg = file := Some arg in
+  Arg.parse opt_list process_arg usage_string;
+  let file =
+    match !file with
+    | Some file -> file
+    | None ->
+        Printf.eprintf "Error: missing file argument\n";
+        Arg.usage opt_list usage_string;
+        exit 1
+  in
   begin
-    try Arg.parse opt_list process_arg usage_string
-    with _ ->
-      Arg.usage opt_list usage_string;
-      exit 1
-  end;
-  let input = read_file !file in
-  let collection = Hbt.Markdown.parse input in
-  if !dump_entities then
-    let entities = Hbt.Collection.entities collection in
-    let () =
+    let open Hbt in
+    let input = read_file file in
+    let collection = Markdown.parse input in
+    if !dump_entities then
+      let entities = Collection.entities collection in
       Array.iter
-        (fun e -> Hbt.Collection.Entity.uri e |> Uri.to_string |> Printf.printf "%s\n")
+        (fun e -> Collection.Entity.uri e |> Uri.to_string |> Printf.printf "%s\n")
         entities
-    in
-    exit 0
-  else
-    let length = Hbt.Collection.length collection in
-    let () = Printf.printf "%s: %d entities\n" !file length in
-    exit 0
+    else
+      let length = Collection.length collection in
+      Printf.printf "%s: %d entities\n" file length
+  end;
+  exit 0
