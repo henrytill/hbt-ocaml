0dfaa8d10eee80ece1251d7592ab9a04f8c9524a
Henry Till <henrytill@gmail.com>
2025-01-24 16:07:05 -0800

    main: rework

--- bin/main.ml
+++ bin/main.ml
@@ -8,74 +8,75 @@ module Args = struct
   }
 end
 
-module type FILE_HANDLER = sig
-  val run : string -> Args.t -> unit
+let read_file file =
+  let ic = open_in file in
+  let ret = In_channel.input_all ic in
+  close_in ic;
+  ret
+
+let update_collection (args : Args.t) : Hbt.Collection.t -> Hbt.Collection.t =
+  match args.mappings_file with
+  | Some file -> read_file file |> Yojson.Basic.from_string |> Hbt.Collection.update_labels
+  | None -> Fun.id
+
+let print_collection (file : string) (args : Args.t) (collection : Hbt.Collection.t) : unit =
+  let open Hbt.Collection in
+  if args.dump_entities then
+    yojson_of_t collection |> Yojson.Safe.pretty_print Format.std_formatter
+  else if args.dump_tags then
+    entities collection
+    |> Array.fold_left (fun acc et -> Entity.labels et |> Label_set.union acc) Label_set.empty
+    |> Label_set.iter (fun l -> Label.to_string l |> Printf.printf "%s\n")
+  else
+    let length = length collection in
+    Printf.printf "%s: %d entities\n" file length
+
+module Make_runner (Collector : sig
+  type t
+
+  val parse : string -> t
+  val to_collection : t -> Hbt.Collection.t
+end) =
+struct
+  let run (file : string) (args : Args.t) : unit =
+    Collector.parse file
+    |> Collector.to_collection
+    |> update_collection args
+    |> print_collection file args
 end
 
-module Markdown : FILE_HANDLER = struct
+module Markdown = Make_runner (struct
   type t = Hbt.Collection.t
 
-  let read_file file =
-    let ic = open_in file in
-    let ret = In_channel.input_all ic in
-    close_in ic;
-    ret
-
-  let update (args : Args.t) : Hbt.Collection.t -> Hbt.Collection.t =
-    match args.mappings_file with
-    | Some file -> read_file file |> Yojson.Basic.from_string |> Hbt.Collection.update_labels
-    | None -> Fun.id
-
   let parse (file : string) : t = read_file file |> Hbt.Markdown.parse
+  let to_collection = Fun.id
+end)
 
-  let print (file : string) (args : Args.t) (collection : t) : unit =
-    let open Hbt.Collection in
-    if args.dump_entities then
-      yojson_of_t collection |> Yojson.Safe.pretty_print Format.std_formatter
-    else if args.dump_tags then
-      entities collection
-      |> Array.fold_left (fun acc et -> Entity.labels et |> Label_set.union acc) Label_set.empty
-      |> Label_set.iter (fun l -> Label.to_string l |> Printf.printf "%s\n")
-    else
-      let length = length collection in
-      Printf.printf "%s: %d entities\n" file length
+let collection_of_posts (posts : Hbt.Pinboard.t list) : Hbt.Collection.t =
+  let ret = Hbt.Collection.make (List.length posts) in
+  List.iter (fun post -> ignore Hbt.Collection.(insert ret (Entity.of_pinboard post))) posts;
+  ret
 
-  let run (file : string) (args : Args.t) : unit = parse file |> update args |> print file args
-end
-
-module Pinboard_shared = struct
+module Xml = Make_runner (struct
   type t = Hbt.Pinboard.t list
 
-  let print (file : string) (args : Args.t) (posts : t) : unit =
-    if args.dump_entities then
-      List.iter (fun p -> Hbt.Pinboard.href p |> Printf.printf "%s\n") posts
-    else if args.dump_tags then
-      ()
-    else
-      let length = List.length posts in
-      Printf.printf "%s: %d entities\n" file length
-end
-
-module Xml : FILE_HANDLER = struct
-  include Pinboard_shared
-
   let parse : string -> t = Hbt.Pinboard.from_xml
-  let run (file : string) (args : Args.t) : unit = parse file |> print file args
-end
+  let to_collection = collection_of_posts
+end)
 
-module Html : FILE_HANDLER = struct
-  include Pinboard_shared
+module Html = Make_runner (struct
+  type t = Hbt.Pinboard.t list
 
   let parse : string -> t = Hbt.Pinboard.from_html
-  let run (file : string) (args : Args.t) : unit = parse file |> print file args
-end
+  let to_collection = collection_of_posts
+end)
 
-module Json : FILE_HANDLER = struct
-  include Pinboard_shared
+module Json = Make_runner (struct
+  type t = Hbt.Pinboard.t list
 
   let parse : string -> t = Hbt.Pinboard.from_json
-  let run (file : string) (args : Args.t) : unit = parse file |> print file args
-end
+  let to_collection = collection_of_posts
+end)
 
 let dump_entities =
   let doc = "Dump entities" in
