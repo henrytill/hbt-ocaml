4d4e3c5435a48d27185edcb3633d18469e8b9e98
Henry Till <henrytill@gmail.com>
2025-08-21 13:33:45 -0700

    test: revamp html import testing strategy

---
+++ test/conversion/gen.ml
@@ -0,0 +1,28 @@
+(* https://dune.readthedocs.io/en/stable/howto/rule-generation.html#using-dynamic-include *)
+
+let generate_rules base =
+  Printf.printf
+    {|
+(rule
+ (package hbt)
+ (action
+  (with-stdout-to
+   %s_out.json
+   (run %%{bin:hbt} --dump ../%s.html))))
+
+(rule
+ (package hbt)
+ (alias runtest)
+ (action
+  (diff ../%s.json %s_out.json)))
+|}
+    base
+    base
+    base
+    base
+
+let () =
+  Sys.readdir ".."
+  |> Array.to_list
+  |> List.filter_map (Filename.chop_suffix_opt ~suffix:".html")
+  |> List.iter generate_rules
