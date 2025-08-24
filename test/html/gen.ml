3f35378c9e1ddc0a245b16ba9b3c908d4a361c36
Henry Till <henrytill@gmail.com>
2025-08-24 14:16:26 -0700

    test: add html export tests

--- test/html/gen.ml
+++ test/html/gen.ml
@@ -21,8 +21,30 @@ let generate_rules base =
     base
     base
 
+let generate_html_export_rules base =
+  Printf.printf
+    {|
+(rule
+ (package hbt)
+ (action
+  (with-stdout-to
+   %s_export_out.html
+   (run %%{bin:hbt} -t html ../%s.html))))
+
+(rule
+ (package hbt)
+ (alias runtest)
+ (action
+  (diff ../export/%s_export.html %s_export_out.html)))
+|}
+    base
+    base
+    base
+    base
+
 let () =
-  Sys.readdir ".."
-  |> Array.to_list
-  |> List.filter_map (Filename.chop_suffix_opt ~suffix:".html")
-  |> List.iter generate_rules
+  let html_files =
+    Sys.readdir ".." |> Array.to_list |> List.filter_map (Filename.chop_suffix_opt ~suffix:".html")
+  in
+  List.iter generate_rules html_files;
+  List.iter generate_html_export_rules html_files
