c06d88b2848697eeb470f943a980d2cfa0fdf88b
Henry Till <henrytill@gmail.com>
2025-08-29 02:52:12 -0700

    Revert "revamp testing strategy"
    This reverts commit dffe506e87058e1a125fa17f7348294f7049ee90.

---
+++ test/gen_pinboard.ml
@@ -0,0 +1,81 @@
+let generate_xml_rule base =
+  let is_problematic = Test_filter.is_problematic_base ~dir:"pinboard" ~ext:".xml" base in
+  let enabled_if =
+    if is_problematic then "\n (enabled_if (= %{profile} failing-tests))" else String.empty
+  in
+  Printf.printf
+    {|
+; Test: %s
+
+(rule
+ (package hbt)
+ (target %s_out.yaml)
+ (deps (:source ../%s.input.xml))
+ (action
+  (with-stdout-to
+   %%{target}
+   (setenv TZ UTC (run %%{bin:hbt} -t yaml %%{source})))))
+
+(rule
+ (package hbt)
+ (deps
+  (:reference ../%s.expected.yaml)
+  (:generated %s_out.yaml))
+ (alias runtest)%s
+ (action
+  (diff %%{reference} %%{generated})))
+|}
+    base
+    base
+    base
+    base
+    base
+    enabled_if
+
+let generate_json_rule base =
+  let is_problematic = Test_filter.is_problematic_base ~dir:"pinboard" ~ext:".json" base in
+  let enabled_if =
+    if is_problematic then "\n (enabled_if (= %{profile} failing-tests))" else String.empty
+  in
+  Printf.printf
+    {|
+; Test: %s
+
+(rule
+ (package hbt)
+ (target %s_out.yaml)
+ (deps (:source ../%s.input.json))
+ (action
+  (with-stdout-to
+   %%{target}
+   (setenv TZ UTC (run %%{bin:hbt} -t yaml %%{source})))))
+
+(rule
+ (package hbt)
+ (deps
+  (:reference ../%s.expected.yaml)
+  (:generated %s_out.yaml))
+ (alias runtest)%s
+ (action
+  (diff %%{reference} %%{generated})))
+|}
+    base
+    base
+    base
+    base
+    base
+    enabled_if
+
+let () =
+  let xml_files =
+    Sys.readdir ".."
+    |> Array.to_list
+    |> List.filter_map (Filename.chop_suffix_opt ~suffix:".input.xml")
+  in
+  let json_files =
+    Sys.readdir ".."
+    |> Array.to_list
+    |> List.filter_map (Filename.chop_suffix_opt ~suffix:".input.json")
+  in
+  List.iter generate_xml_rule xml_files;
+  List.iter generate_json_rule json_files
