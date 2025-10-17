c761b772a47e2f646ac92486dd39e64ff9f373a5
Henry Till <henrytill@gmail.com>
2025-10-17 15:33:34 -0700

    embed.ml: tweak

--- lib/embed.ml
+++ lib/embed.ml
@@ -1,7 +1,7 @@
 let () =
   let args = Sys.argv in
   let input_file = args.(1) in
-  let name = Filename.remove_extension (Filename.basename input_file) in
+  let name = Filename.(remove_extension (basename input_file)) in
   let ic = open_in input_file in
   let finally () = close_in ic in
   let content = Fun.protect ~finally (fun () -> really_input_string ic (in_channel_length ic)) in
