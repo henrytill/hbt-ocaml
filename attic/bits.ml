d5b59a13d4f3eaac40c7a7ab18581a676593d0a9
Henry Till <henrytill@gmail.com>
2026-02-28 14:28:18 -0800

    attic: remove Bits

--- attic/bits.ml
+++
@@ -1,6 +0,0 @@
-let popcount x =
-  let x = x land 0xFFFFFFFF in
-  let x = x - ((x lsr 1) land 0x55555555) in
-  let x = (x land 0x33333333) + ((x lsr 2) land 0x33333333) in
-  let x = (x + (x lsr 4)) land 0x0f0f0f0f in
-  ((x * 0x01010101) lsr 24) land 0xFF
--- attic/bits.mli
+++
@@ -1,2 +0,0 @@
-val popcount : int -> int
-(** [popcount x] returns the number of 1-bits in the low 32 bits of [x]. *)
